9fd12366bcbb31bfa344bc3a8f9b2481a565ae46
Antony Vasiliev <tony.vasiliev8@gmail.com>
2019-10-24 23:47:48 -0400

    started working on enemies

--- triangles/Enemy.js
+++ triangles/Enemy.js
@@ -1,1 +1,21 @@
-﻿
+﻿class Enemy{
+    enemy = {
+        health: null,
+        damage: null,
+        image: null,
+        xPos: null,
+        yPos: null
+    };
+
+    constructor(img, x, y){
+        
+    }
+
+    RandomSpawn(){
+
+    }
+
+    TrackPlayer(){
+
+    }
+}
